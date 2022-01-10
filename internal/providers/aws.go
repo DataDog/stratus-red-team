3839e37dfc5c88aacaf701875555a8ec0b2b84d0
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-01-10 18:08:23 +0100

    Remove buggy code

--- internal/providers/aws.go
+++ internal/providers/aws.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"log"
 )
 
@@ -12,16 +11,16 @@ var isAuthenticated = false
 var hasDeterminedIfAuthenticated = false
 
 func init() {
-	aws, _ := config.LoadDefaultConfig(context.TODO())
+	/*aws, _ := config.LoadDefaultConfig(context.TODO())
 	stsClient := sts.NewFromConfig(aws)
 	_, err := stsClient.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
-	isAuthenticated = err == nil
+	isAuthenticated = err == nil*/
 }
 
 func GetAWSProvider() aws.Config {
-	if !isAuthenticated {
+	/*if !isAuthenticated {
 		log.Fatal("You are not authenticated to AWS, or have not set your default AWS region.")
-	}
+	}*/
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
