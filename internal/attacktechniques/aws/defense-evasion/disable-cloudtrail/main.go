0c0234f2c131276d15c65814298e1987c74b1cd5
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-01-18 13:21:57 +0100

    Add TTP: disabling CloudTrail

---
+++ internal/attacktechniques/aws/defense-evasion/disable-cloudtrail/main.go
@@ -0,0 +1,50 @@
+package aws
+
+import (
+	"context"
+	_ "embed"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
+	"github.com/datadog/stratus-red-team/internal/providers"
+	"github.com/datadog/stratus-red-team/pkg/stratus"
+	"github.com/datadog/stratus-red-team/pkg/stratus/mitreattack"
+	"log"
+)
+
+//go:embed main.tf
+var tf []byte
+
+func init() {
+	stratus.GetRegistry().RegisterAttackTechnique(&stratus.AttackTechnique{
+		ID:                 "aws.defense-evasion.stop-cloudtrail",
+		FriendlyName:       "Stops a CloudTrail trail",
+		Platform:           stratus.AWS,
+		MitreAttackTactics: []mitreattack.Tactic{mitreattack.DefenseEvasion},
+		Description: `
+Stops a CloudTrail trail from logging.
+
+Warm-up: Creates a CloudTrail trail.
+Detonation: Calls cloudtrail:StopLogging
+`,
+		PrerequisitesTerraformCode: tf,
+		Detonate:                   detonate,
+	})
+}
+
+func detonate(params map[string]string) error {
+	cloudtrailClient := cloudtrail.NewFromConfig(providers.AWS().GetConnection())
+	trailName := params["cloudtrail_trail_name"]
+
+	log.Println("Stopping CloudTrail trail " + trailName)
+
+	_, err := cloudtrailClient.StopLogging(context.Background(), &cloudtrail.StopLoggingInput{
+		Name: aws.String(trailName),
+	})
+
+	if err != nil {
+		return errors.New("unable to stop CloudTrail logging: " + err.Error())
+	}
+
+	return nil
+}
---
+++ internal/attacktechniques/aws/defense-evasion/disable-cloudtrail/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.71.0"
+    }
+  }
+}
+provider "aws" {
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  default_tags {
+    tags = {
+      StratusRedTeam = true
+    }
+  }
+}
+
+resource "aws_cloudtrail" "trail" {
+  name = "my-cloudtrail-trail"
+  s3_bucket_name = aws_s3_bucket.cloudtrail.id
+}
+
+resource "random_string" "suffix" {
+  length    = 16
+  min_lower = 16
+  special   = false
+}
+
+locals {
+  bucket-name = "my-cloudtrail-bucket-${random_string.suffix.result}"
+}
+resource "aws_s3_bucket" "cloudtrail" {
+  bucket        = local.bucket-name
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::${local.bucket-name}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${local.bucket-name}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+output "cloudtrail_trail_name" {
+  value = aws_cloudtrail.trail.name
+}
