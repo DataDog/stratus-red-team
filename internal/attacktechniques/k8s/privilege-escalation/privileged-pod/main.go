f3e92f81a0030696dfc4d313bdce223db09daa30
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-02-07 13:22:53 +0100

    Add detection docs for k8s.privilege-escalation.privileged-pod

--- internal/attacktechniques/k8s/privilege-escalation/privileged-pod/main.go
+++ internal/attacktechniques/k8s/privilege-escalation/privileged-pod/main.go
@@ -18,6 +18,8 @@ import (
 //go:embed main.tf
 var tf []byte
 
+const codeBlock = "```"
+
 func init() {
 	stratus.GetRegistry().RegisterAttackTechnique(&stratus.AttackTechnique{
 		ID:                 "k8s.privilege-escalation.privileged-pod",
@@ -41,6 +43,45 @@ Detonation:
 
 - Create a privileged busybox pod
 `,
+		Detection: `
+Using Kubernetes API server audit logs, looking for pod creation events with <code>requestObject.spec.containers[*].securityContext.privileged</code>
+set to <code>true</code>.
+
+Sample event (shortened):
+
+` + codeBlock + `json hl_lines="11 19 26"
+{
+	"objectRef": {
+		"resource": "pods",
+		"name": "k8s.privilege-escalation.privileged-pod",
+		"apiVersion": "v1"
+	},
+	"http": {
+		"url_details": {
+			"path": "/api/v1/namespaces/stratus-red-team-umusjhhg/pods"
+		},
+		"method": "create",
+		"status_code": 201
+	},
+	"stage": "ResponseComplete",
+	"kind": "Event",
+	"level": "RequestResponse",
+	"requestURI": "/api/v1/namespaces/stratus-red-team-umusjhhg/pods",
+	"requestObject": {
+		"kind": "Pod",
+		"spec": {
+			"containers": [{
+				"image": "busybox:stable",
+				"args": ["while true; do sleep 3600; done"],
+				"command": ["sh", "-c"],
+				"securityContext": {
+					"privileged": true
+				}
+			}]
+		}
+	}
+}
+` + codeBlock,
 		PrerequisitesTerraformCode: tf,
 		Detonate:                   detonate,
 		Revert:                     revert,
