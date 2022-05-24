4a8e0347b401ebecf053330f134af8e5c0ac16b6
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-05-24 11:28:26 +0200

    Move EnsureAuthenticated method to avoid cyclic dependency

--- internal/providers/main.go
+++ internal/providers/main.go
@@ -1,32 +1,9 @@
 package providers
 
 import (
-	"errors"
-	"github.com/datadog/stratus-red-team/pkg/stratus"
 	"github.com/google/uuid"
 )
 
 const StratusUserAgent = "stratus-red-team"
 
 var UniqueExecutionId = uuid.New()
-
-// EnsureAuthenticated ensures that the current user is properly authenticated against a specific platform
-func EnsureAuthenticated(platform stratus.Platform) error {
-	switch platform {
-	case stratus.AWS:
-		if !AWS().IsAuthenticatedAgainstAWS() {
-			return errors.New("you are not authenticated against AWS, or you have not set your region. " +
-				"Make sure you are authenticated against AWS, and you have a default region set in your AWS config " +
-				"or environment (export AWS_DEFAULT_REGION=us-east-1)")
-		}
-	case stratus.Kubernetes:
-		if !K8s().IsAuthenticated() {
-			return errors.New("You do not have a kubeconfig set up, or you do not have proper permissions for " +
-				"this cluster. Make sure you have proper credentials set in " + GetKubeConfigPath())
-		}
-	default:
-		return errors.New("unhandled platform " + string(platform))
-	}
-
-	return nil
-}
