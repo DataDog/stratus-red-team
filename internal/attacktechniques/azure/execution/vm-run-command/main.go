b236e54205fdd241ab1c03c00ebc13dfa2b83730
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-06-01 13:55:22 +0200

    [azure] Add detection information for azure.execution.vm-run-command

--- internal/attacktechniques/azure/execution/vm-run-command/main.go
+++ internal/attacktechniques/azure/execution/vm-run-command/main.go
@@ -18,6 +18,7 @@ import (
 var tf []byte
 
 func init() {
+	const codeBlock = "```"
 	stratus.GetRegistry().RegisterAttackTechnique(&stratus.AttackTechnique{
 		ID:           "azure.execution.vm-run-command",
 		FriendlyName: "Execute Commands on Virtual Machine using Run Command",
@@ -41,7 +42,46 @@ Detonation:
 
 - Invoke a RunCommand on the target virtual machine
 `,
-		Detection:                  "Identify `Microsoft.Compute/virtualMachines/runCommand/action` events in Azure Activity logs",
+		Detection: `
+Identify <code>Microsoft.Compute/virtualMachines/runCommand/action</code> 
+and <code>Microsoft.Compute/virtualMachines/runCommands/write</code> events in Azure Activity logs.
+
+Sample event (redacted for clarity):
+
+` + codeBlock + `json hl_lines="7"
+{
+    "caller": "you@domain.tld",
+	"eventTimestamp": "2022-06-01T11:39:35.6986539Z",
+    "id": "/subscriptions/<your-subscription-id>/resourceGroups/rg-4x3tj2hb/providers/Microsoft.Compute/virtualMachines/vm-4x3tj2hb/events/25235036-3b0c-46e7-97d0-5bea476a6ab8/ticks/637896803756986539",
+    "level": "Informational",
+    "operationName": {
+        "value": "Microsoft.Compute/virtualMachines/runCommand/action",
+        "localizedValue": "Run Command on Virtual Machine"
+    },
+    "resourceGroupName": "rg-4x3tj2hb",
+    "resourceProviderName": {
+        "value": "Microsoft.Compute",
+        "localizedValue": "Microsoft.Compute"
+    },
+    "resourceType": {
+        "value": "Microsoft.Compute/virtualMachines",
+        "localizedValue": "Microsoft.Compute/virtualMachines"
+    },
+    "resourceId": "/subscriptions/<your-subscription-id>/resourceGroups/rg-4x3tj2hb/providers/Microsoft.Compute/virtualMachines/vm-4x3tj2hb",
+    "status": {
+        "value": "Succeeded",
+        "localizedValue": "Succeeded"
+    },
+    "properties": {
+        "eventCategory": "Administrative",
+        "entity": "/subscriptions/<your-subscription-id>/resourceGroups/rg-4x3tj2hb/providers/Microsoft.Compute/virtualMachines/vm-4x3tj2hb",
+        "message": "Microsoft.Compute/virtualMachines/runCommand/action",
+        "hierarchy": "<your-tenant-id>/<your-subscription-id>"
+    },
+    "relatedEvents": []
+}
+` + codeBlock + `
+`,
 		Platform:                   stratus.Azure,
 		IsSlow:                     true,
 		IsIdempotent:               true,
@@ -73,9 +113,9 @@ func detonate(params map[string]string) error {
 	}
 
 	log.Println("Waiting for command to be run on the VM")
-	ctxWithTimeout, done := context.WithTimeout(context.Background(), 60*time.Second)
+	ctxWithTimeout, done := context.WithTimeout(context.Background(), 60*3*time.Second) // This can sometimes be quite slow
 	defer done()
-	commandResult, err := commandCreation.PollUntilDone(ctxWithTimeout, &runtime.PollUntilDoneOptions{Frequency: 2 * time.Minute})
+	commandResult, err := commandCreation.PollUntilDone(ctxWithTimeout, &runtime.PollUntilDoneOptions{Frequency: 2 * time.Second})
 	if err != nil {
 		return errors.New("unable to retrieve the output of the command ran on the virtual machine: " + err.Error())
 	}
