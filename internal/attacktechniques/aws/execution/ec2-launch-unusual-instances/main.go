3702f1dbbf0c4202b07f20e6fb6010999a702427
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-07-13 07:07:14 +0200

    ec2-launch-unusual-instances: Use non-default VPC and retrieve AMI ID using Terraform

--- internal/attacktechniques/aws/execution/ec2-launch-unusual-instances/main.go
+++ internal/attacktechniques/aws/execution/ec2-launch-unusual-instances/main.go
@@ -4,13 +4,6 @@ import (
 	"context"
 	_ "embed"
 	"errors"
-	"fmt"
-	"github.com/aws/smithy-go/ptr"
-	"log"
-	"sort"
-	"strings"
-	"time"
-
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -19,6 +12,8 @@ import (
 	"github.com/datadog/stratus-red-team/internal/providers"
 	"github.com/datadog/stratus-red-team/pkg/stratus"
 	"github.com/datadog/stratus-red-team/pkg/stratus/mitreattack"
+	"log"
+	"strings"
 )
 
 //go:embed main.tf
@@ -59,21 +54,20 @@ func detonate(params map[string]string) error {
 	ctx := context.Background()
 	awsConnection := providers.AWS().GetConnection()
 
-	// get ami image id before assuming role with limited access
-	amiId, err := getAmazonLinuxAmiId(&ctx, &awsConnection)
-	if err != nil {
-		return fmt.Errorf("could not get a valid EC2 AMI id: %v", err)
-	}
+	amiId := params["ami_id"]
+	roleArn := params["role_arn"]
+	subnetId := params["subnet_id"]
 
 	stsClient := sts.NewFromConfig(awsConnection)
-	awsConnection.Credentials = aws.NewCredentialsCache(stscreds.NewAssumeRoleProvider(stsClient, params["role_arn"]))
+	awsConnection.Credentials = aws.NewCredentialsCache(stscreds.NewAssumeRoleProvider(stsClient, roleArn))
 	ec2Client := ec2.NewFromConfig(awsConnection)
 
 	log.Printf("Running %d instances of type %s\n", numInstances, string(instanceType))
-	_, err = ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
-		ImageId:      &amiId,
-		MinCount:     ptr.Int32(numInstances),
-		MaxCount:     ptr.Int32(numInstances),
+	_, err := ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
+		ImageId:      aws.String(amiId),
+		SubnetId:     aws.String(subnetId),
+		MinCount:     aws.Int32(numInstances),
+		MaxCount:     aws.Int32(numInstances),
 		InstanceType: instanceType, // types.InstanceTypeP4d24xlarge,
 	})
 
@@ -90,48 +84,4 @@ func detonate(params map[string]string) error {
 	log.Println("Got an access denied error as expected")
 
 	return nil
-}
-
-// getAmazonLinuxAmiId returns the most current Amazon Linux AMI image id (as AMIs are region specific)
-func getAmazonLinuxAmiId(ctx *context.Context, config *aws.Config) (string, error) {
-	ec2Client := ec2.NewFromConfig(*config)
-
-	filterName, filterArch, filterRootDev := "name", "architecture", "root-device-type"
-	input := ec2.DescribeImagesInput{
-		Filters: []types.Filter{
-			{
-				Name:   &filterName,
-				Values: []string{"amzn2-ami-kernel-*"},
-			},
-			{
-				Name:   &filterArch,
-				Values: []string{"x86_64"},
-			},
-			{
-				Name:   &filterRootDev,
-				Values: []string{"ebs"},
-			},
-			{
-				Name:   &filterRootDev,
-				Values: []string{"ebs"},
-			},
-		},
-	}
-	output, err := ec2Client.DescribeImages(*ctx, &input)
-	if err != nil {
-		return "", fmt.Errorf("could not get ec2 images list from aws: %v", err)
-	}
-
-	images := output.Images
-	sort.Slice(images, func(i, j int) bool {
-		ti, _ := time.Parse(time.RFC3339, *images[i].CreationDate)
-		tj, _ := time.Parse(time.RFC3339, *images[j].CreationDate)
-		return ti.After(tj)
-	})
-
-	if len(images) == 0 {
-		return "", fmt.Errorf("error determining latest ec2 image to use, image list is empty")
-	}
-
-	return *images[0].ImageId, nil
 }
