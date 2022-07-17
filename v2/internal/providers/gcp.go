6c8bad53d33d9abac75f31ad45392a91ac5abe4b
rileydakota <dakotariley2@gmail.com>
2022-07-17 13:42:06 -0400

    WIP: add gcp auth check and project env var check

--- v2/internal/providers/gcp.go
+++ v2/internal/providers/gcp.go
@@ -1,7 +1,60 @@
 package providers
 
-//func Gcp(){}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/google/uuid"
+	"google.golang.org/api/iam/v1"
+)
+
+// TF and GCP defines multiple environment variables for this
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#full-reference
+
+func IsProjectEnvVarSet() bool {
+	gcloudProjEnvVars := []string{
+		"GOOGLE_PROJECT",
+		"GOOGLE_CLOUD_PROJECT",
+		"GCLOUD_PROJECT",
+		"CLOUDSDK_CORE_PROJECT",
+	}
+
+	for _, key := range gcloudProjEnvVars {
+		if len(os.Getenv(key)) != 0 {
+			log.Println(key + " var set!")
+			return true
+		}
+	}
+	return false
+}
+
+type GcpProvider struct {
+	UniqueCorrelationId uuid.UUID
+}
+
+var gcpProvider = GcpProvider{
+	UniqueCorrelationId: UniqueExecutionId,
+}
+
+func GCP() *GcpProvider {
+	return &gcpProvider
+}
+
+//func Gcp() {}
 
 //func GetClientParams(){}
 
-//func IsAuthenticated() bool {}
+func (m *GcpProvider) IsAuthenticated() bool {
+	ctx := context.Background()
+	_, err := iam.NewService(ctx)
+	if err != nil {
+		log.Println("Authentication Error:" + err.Error())
+		return false
+	}
+	if !IsProjectEnvVarSet() {
+		return false
+	}
+
+	return true
+}
