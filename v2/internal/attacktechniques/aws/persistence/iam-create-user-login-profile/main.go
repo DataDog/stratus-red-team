3d90753c1161c5fec6b5c161d7d91ab9f687e6cd
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-07-22 10:44:57 +0200

    rename incorrect import

--- v2/internal/attacktechniques/aws/persistence/iam-create-user-login-profile/main.go
+++ v2/internal/attacktechniques/aws/persistence/iam-create-user-login-profile/main.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/datadog/stratus-red-team/v2/internal/providers"
-	utils2 "github.com/datadog/stratus-red-team/v2/internal/utils"
+	"github.com/datadog/stratus-red-team/v2/internal/utils"
 	"github.com/datadog/stratus-red-team/v2/pkg/stratus"
 	"github.com/datadog/stratus-red-team/v2/pkg/stratus/mitreattack"
 	"log"
@@ -48,7 +48,7 @@ In particular, it's suspicious when these events occur on IAM users intended to 
 func detonate(params map[string]string) error {
 	iamClient := iam.NewFromConfig(providers.AWS().GetConnection())
 	userName := params["user_name"]
-	password := utils2.RandomString(16) + ".#1Aa" // extra characters to ensure we meet password requirements, no matter the password policy
+	password := utils.RandomString(16) + ".#1Aa" // extra characters to ensure we meet password requirements, no matter the password policy
 
 	log.Println("Creating a login profile on IAM user " + userName)
 	_, err := iamClient.CreateLoginProfile(context.Background(), &iam.CreateLoginProfileInput{
@@ -60,7 +60,7 @@ func detonate(params map[string]string) error {
 		return errors.New("unable to create IAM login profile: " + err.Error())
 	}
 
-	accountId, _ := utils2.GetCurrentAccountId(providers.AWS().GetConnection())
+	accountId, _ := utils.GetCurrentAccountId(providers.AWS().GetConnection())
 	log.Println("Created a login profile with password " + password)
 	loginUrl := "https://" + accountId + ".signin.aws.amazon.com/console"
 	log.Println("You can log in at: " + loginUrl)
