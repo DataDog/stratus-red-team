2c511b7394ce9d32239d572c48258a44d7b9c646
Ryan Cobb <rcobb@secureworks.com>
2022-08-04 08:55:30 -0400

    Update docs to link to Azure Threat Research Matrix

--- v2/internal/attacktechniques/azure/execution/vm-run-command/main.go
+++ v2/internal/attacktechniques/azure/execution/vm-run-command/main.go
@@ -32,7 +32,7 @@ References:
 
 - https://docs.microsoft.com/en-us/azure/virtual-machines/windows/run-command
 - https://docs.microsoft.com/en-us/azure/virtual-machines/linux/run-command
-- https://github.com/hausec/Azure-Attack-Matrix/blob/main/Execution/AZT201/AZT201-1.md
+- https://microsoft.github.io/Azure-Threat-Research-Matrix/Execution/AZT301/AZT301-1/
 
 Warm-up: 
 
