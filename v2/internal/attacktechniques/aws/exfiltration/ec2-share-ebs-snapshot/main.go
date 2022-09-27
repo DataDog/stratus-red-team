fc2719261a4605c45cf817dfb0696be216bc43b8
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-09-27 08:43:15 +0000

    Add information about CloudTrail events for EBS exfiltration (#171)
    * Add information about CloudTrail events for EBS exfiltration
    
    * Generate docs

--- v2/internal/attacktechniques/aws/exfiltration/ec2-share-ebs-snapshot/main.go
+++ v2/internal/attacktechniques/aws/exfiltration/ec2-share-ebs-snapshot/main.go
@@ -49,6 +49,34 @@ that the EBS snapshot was shared with a new or unknown AWS account, such as:
 
 An attacker can also make an EBS snapshot completely public. In this case, the <code>item</code> entry 
 will look like <code>{"groups":"all"}</code>. 
+
+When an attacker copies the snapshot to their own AWS account or creates an EBS volume for it, the <code>SharedSnapshotCopyInitiated</code> (respectively <code>SharedSnapshotVolumeCreated</code>) event is logged (see [AWS docs](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-modifying-snapshot-permissions.html#shared-snapshot-cloudtrail-logging)). 
+In that case, <code>userIdentity.accountId</code> contains the attacker's account ID and <code>recipientAccountId</code> contains the victim's account ID where the snapshot was originally created.
+
+<pre><code>{
+  "userIdentity": {
+    "invokedBy": "ec2.amazonaws.com",
+    "type": "AWSAccount",
+    "accountId": "999999999999"
+  },
+  "eventSource": "ec2.amazonaws.com",
+  "eventVersion": "1.08",
+  "eventTime": "2022-09-27T07:58:49Z",
+  "service": "cloudtrail",
+  "eventName": "SharedSnapshotCopyInitiated",
+  "eventType": "AwsServiceEvent",
+  "eventCategory": "Management",
+  "awsRegion": "us-east-1",
+    "serviceEventDetails": {
+    "snapshotId": "snap-12345"
+  },
+  "readOnly": false,
+  "managementEvent": true,
+  "recipientAccountId": "111111111111"
+ }
+ </code></pre>
+ 
+ Note that detonating this attack technique with Stratus Red Team does *not* simulate an attacker accessing the snapshot from their account (only sharing it publicly from your account).
 `,
 		PrerequisitesTerraformCode: tf,
 		Detonate:                   detonate,
