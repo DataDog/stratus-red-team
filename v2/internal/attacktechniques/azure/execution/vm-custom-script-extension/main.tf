c3e37e5deb64d335680ee8fd8192215bdbdbdb47
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2023-01-30 20:55:30 +0100

    Fix resource name lengths in Azure attack techniques (closes #301)

--- v2/internal/attacktechniques/azure/execution/vm-custom-script-extension/main.tf
+++ v2/internal/attacktechniques/azure/execution/vm-custom-script-extension/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 locals {
-  resource_prefix = "stratus-red-team-vmcse" # stratus red team vm custom script extension
+  resource_prefix = "srt-vmcse" # stratus red team vm custom script extension
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -75,7 +75,7 @@ resource "azurerm_network_interface" "lab_nic" {
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 resource "azurerm_windows_virtual_machine" "lab_windows_vm" {
-  name                = "${local.resource_prefix}-vm-${random_string.lab_name.result}"
+  name                = "srt-vm-vmcse" # at most 15 characters
   resource_group_name = azurerm_resource_group.lab_environment.name
   location            = azurerm_resource_group.lab_environment.location
   size                = "Standard_F2"
