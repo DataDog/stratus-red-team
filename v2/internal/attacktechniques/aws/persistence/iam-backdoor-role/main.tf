a53f67ece6a82deebeacf8a5539fefef099211b6
Mrugank Patankar <mrugank@canva.com>
2023-06-07 15:43:36 +1000

    more white space fixes

--- v2/internal/attacktechniques/aws/persistence/iam-backdoor-role/main.tf
+++ v2/internal/attacktechniques/aws/persistence/iam-backdoor-role/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
   skip_region_validation      = true
   skip_credentials_validation = true
   skip_get_ec2_platforms      = true
-  }
+}
 
 locals {
   resource_prefix = "stratus-red-team-backdoor-r"
