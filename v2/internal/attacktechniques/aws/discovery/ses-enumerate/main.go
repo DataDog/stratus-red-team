3808483f8bfa5f76fa88cd8a16e212b86f0f0ef2
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2024-04-04 12:18:22 +0200

    Only use SES (not SESv2) API calls following recommendation from an IR practitioner

--- v2/internal/attacktechniques/aws/discovery/ses-enumerate/main.go
+++ v2/internal/attacktechniques/aws/discovery/ses-enumerate/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/sesv2"
 	"github.com/datadog/stratus-red-team/v2/pkg/stratus"
 	"github.com/datadog/stratus-red-team/v2/pkg/stratus/mitreattack"
 	"log"
@@ -22,7 +21,7 @@ Simulates an attacker enumerating SES. Attackers frequently use this enumeration
 Warm-up: None.
 
 Detonation: 
-TODO
+- Perform <code>ses:GetAccountSendingEnabled</code> to check if SES sending is enabled.
 - Perform <code>ses:GetSendQuota</code> to discover the current [email sending quotas](https://docs.aws.amazon.com/ses/latest/APIReference/API_GetSendQuota.html).
 - Perform <code>ses:ListIdentities</code> to discover the list of [identities](https://docs.aws.amazon.com/ses/latest/APIReference/API_ListIdentities.html) in the account.
 - When identities are found, use <code>ses:GetIdentityVerificationAttributes</code> to discover the [verification status](https://docs.aws.amazon.com/ses/latest/APIReference/API_GetIdentityVerificationAttributes.html) of each identity.
@@ -35,7 +34,8 @@ References:
 - https://www.invictus-ir.com/news/the-curious-case-of-dangerdev-protonmail-me
 `,
 		Detection: `
-Through CloudTrail's <code>GetSendQuota</code> and <code>ListIdentities</code> events.
+Through CloudTrail's <code>GetAccountSendingEnabled</code>, <code>GetSendQuota</code> and <code>ListIdentities</code> events. 
+These can be considered suspicious especially when performed by a long-lived access key, or when the calls span across multiple regions.
 `,
 		Platform:           stratus.AWS,
 		IsIdempotent:       true,
@@ -47,9 +47,8 @@ Through CloudTrail's <code>GetSendQuota</code> and <code>ListIdentities</code> e
 func detonate(_ map[string]string, providers stratus.CloudProviders) error {
 	awsConnection := providers.AWS().GetConnection()
 	sesClient := ses.NewFromConfig(awsConnection)
-	sesV2Client := sesv2.NewFromConfig(awsConnection)
 
-	/*log.Println("Checking is SES e-mail sending is enabled in the current region")
+	log.Println("Checking is SES e-mail sending is enabled in the current region")
 	result, err := sesClient.GetAccountSendingEnabled(context.Background(), &ses.GetAccountSendingEnabledInput{})
 	if err != nil {
 		return fmt.Errorf("unable to check if SES sending is enabled: %w", err)
@@ -58,24 +57,7 @@ func detonate(_ map[string]string, providers stratus.CloudProviders) error {
 		log.Println("SES e-mail sending is enabled")
 	} else {
 		log.Println("SES e-mail sending is disabled")
-	}*/
-
-	log.Println("Checking if SES e-mail sending is enabled in the current region, and quotas in place")
-	account, err := sesV2Client.GetAccount(context.Background(), &sesv2.GetAccountInput{})
-	if err != nil {
-		return fmt.Errorf("unable to get SES account details: %w", err)
 	}
-	if account.SendingEnabled {
-		log.Println("SES e-mail sending is enabled in the current region")
-	} else {
-		log.Println("SES e-mail sending is disabled in the current region")
-	}
-	if account.ProductionAccessEnabled {
-		log.Println("SES is in production mode!")
-	} else {
-		log.Println("SES is in sandbox mode")
-	}
-	log.Printf("SES quotas: max24hoursend: %d, maxsendrate: %d, sentlast24hours: %d\n", int(account.SendQuota.Max24HourSend), int(account.SendQuota.MaxSendRate), int(account.SendQuota.SentLast24Hours))
 
 	log.Println("Enumerating verified SES identities using ses:ListIdentities")
 	identities, err := sesClient.ListIdentities(context.Background(), &ses.ListIdentitiesInput{})
@@ -98,13 +80,13 @@ func detonate(_ map[string]string, providers stratus.CloudProviders) error {
 		}
 	}
 
-	/*log.Println("Enumerating SES quotas")
+	log.Println("Enumerating SES quotas")
 	quotas, err := sesClient.GetSendQuota(context.Background(), &ses.GetSendQuotaInput{})
 	if err != nil {
 		return fmt.Errorf("unable to get SES quotas: %w", err)
 	}
 
-	log.Printf("Current quotas: max24hoursend: %d, maxsendrate: %d, sentlast24hours: %d\n", int(quotas.Max24HourSend), int(quotas.MaxSendRate), int(quotas.SentLast24Hours))*/
+	log.Printf("Current quotas: max24hoursend: %d, maxsendrate: %d, sentlast24hours: %d\n", int(quotas.Max24HourSend), int(quotas.MaxSendRate), int(quotas.SentLast24Hours))
 
 	return nil
 }
