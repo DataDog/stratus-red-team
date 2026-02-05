7555d06afe39468b2f272524308ea2d043750b6b
Jonah Feldman <jonah.feldman@datadoghq.com>
2026-02-05 15:43:19 -0500

    tf fmt

--- v2/internal/attacktechniques/azure/impact/blob-ransomware-client-encryption-scope/main.tf
+++ v2/internal/attacktechniques/azure/impact/blob-ransomware-client-encryption-scope/main.tf
@@ -357,14 +357,14 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                        = local.key_vault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  enable_rbac_authorization   = true
+  name                       = local.key_vault_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  enable_rbac_authorization  = true
 }
 
 resource "azurerm_role_assignment" "storage_keyvault_crypto" {
